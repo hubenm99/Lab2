cbc4fbcae438cadb965e54ba2e5eca5f7d06a79e
Nick Huber <hubenm99@gmail.com>
2022-08-31 16:54:08 -0700

    This is the finished lab

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,43 @@
+// Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float A;
+	float B;
+	float X;
+
+	cout << "Hello I will solve Ax+B=0 for x" << endl;
+
+	cout << endl << "Please enter a value for A: ";
+	cin >> A;
+
+	cout << "Please enter a value for B: ";
+	cin >> B;
+
+
+	cout << endl << "Solving " << A << "x+" << B << "=0 for x." << endl;
+
+	cout <<  endl << "The answer is:";
+
+	X = (0 - B) / A;
+
+	cout << endl << "X = " << X;
+	
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
